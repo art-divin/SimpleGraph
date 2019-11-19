c243fd04a8ea849741cee431f8b8e88ef4515f35
Ruslan Alikhamov <ruslan.alikhamov@cuciniale.com>
2019-11-19 19:27:21 +0300

    touched Backend fmwk

---
+++ Backend/Backend/Backend.h
@@ -0,0 +1,19 @@
+//
+//  Backend.h
+//  Backend
+//
+//  Created by Ruslan Alikhamov on 19/11/2019.
+//  Copyright © 2019 Ruslan Alikhamov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Backend.
+FOUNDATION_EXPORT double BackendVersionNumber;
+
+//! Project version string for Backend.
+FOUNDATION_EXPORT const unsigned char BackendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Backend/PublicHeader.h>
+
+
